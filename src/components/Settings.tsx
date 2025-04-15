995777b9acf5e4762430d28d789701e0c5d51add
Nicholas <nap44794@uga.edu>
2025-04-14 22:34:18 -0400

    feature: implement basic settings page

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,12 +1,15 @@
 "use client";
 import Link from "next/link";
-import { doCredentialLogin } from "../app/actions";
+import { doCredentialLogin, doSaveSettings } from "../app/actions";
 import { useState, FormEvent } from "react";
 import { useRouter } from "next/navigation";
+import { AmenityCheckbox } from './AmenityCheckbox';
 
-const Login = () => {
+const Settings = () => {
   const router = useRouter();
   const [error, setError] = useState<string>("");
+  const [showElevators, setShowElevators] = useState(true);
+  const [showBathrooms, setShowBathrooms] = useState(false);
 
   async function onSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
@@ -14,13 +17,12 @@ const Login = () => {
     try {
       const formData = new FormData(event.currentTarget);
 
-      const response = await doCredentialLogin(formData);
+      const response = await doSaveSettings(formData);
 
       if (response?.error) {
         console.error(response.error);
         setError(response.error.message || "An error occurred");
       } else {
-        router.push("/");
       }
     } catch (e: any) {
       console.error(e);
@@ -30,58 +32,32 @@ const Login = () => {
 
   return (
     <div className='ShowItemList'>
-    <div className="grid mt-8 justify-items-center"> 
-      <div className="shadow-lg p-5 rounded-lg border-t-4 bg-white border-red-700">
-       <h1 className="text-xl text-slate-600 font-bold my-4">Login</h1>
-      {error && <div className="text-lg text-red-500">{error}</div>}
-      <form
-  onSubmit={onSubmit}
-  className="my-8 max-w-md mx-auto flex flex-col gap-4 border p-6 border-gray-300 rounded-md shadow-sm bg-white"
->
-  <div className="flex flex-col">
-    <label htmlFor="email" className="mb-1 text-sm font-medium text-gray-700">
-      Email Address
-    </label>
-    <input
-      className="border border-gray-400 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
-      type="email"
-      name="email"
-      id="email"
-      placeholder="Email"
-      required
-    />
-  </div>
+        <div className="grid mt-8 justify-items-center"> 
+            <div className="shadow-lg p-5 rounded-lg border-t-4 bg-white border-red-700">
+                <h1 className="text-xl text-slate-600 font-bold my-4">Settings</h1>
+                {error && <div className="text-lg text-red-500">{error}</div>}
+                <form
+                    onSubmit={onSubmit}
+                    className="my-8 max-w-md mx-auto flex flex-col gap-4 border p-6 border-gray-300 rounded-md shadow-sm bg-white"
+                >
 
-  <div className="flex flex-col">
-    <label htmlFor="password" className="mb-1 text-sm font-medium text-gray-700">
-      Password
-    </label>
-    <input
-      className="border border-gray-400 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-red-500"
-      type="password"
-      name="password"
-      id="password"
-      placeholder="Password"
-      required
-    />
-  </div>
+                    <AmenityCheckbox id="elevators" label="Elevators" name="amenities" />
+                    <AmenityCheckbox id="stairs" label="Stairs" name="amenities" />
+                    <AmenityCheckbox id="ramps" label="Ramps" name="amenities" />
+                    <AmenityCheckbox id="genderNeutralBathrooms" label="Gender Neutral Bathrooms" name="amenities" />
+                    <AmenityCheckbox id="accessibleBathrooms" label="Accessible Bathrooms" name="amenities" />
 
-  <button
-    type="submit"
-    className="bg-red-700 text-white rounded px-4 py-2 mt-2 hover:bg-red-800 transition"
-  >
-    Login
-  </button>
-</form>
-
-      <p className="my-3 text-center">
-        Don't you have an account?
-        <Link href="signup" className="mx-2 underline">Signup</Link>
-      </p>
-    </div>
-    </div>
+                    <button
+                        type="submit"
+                        className="bg-red-700 text-white rounded px-4 py-2 mt-2 hover:bg-red-800 transition"
+                    >
+                        Save    
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
   );
 };
 
-export default Login;
+export default Settings;
